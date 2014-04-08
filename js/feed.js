67725f236615c8f7618fd836b5ee8f7bc70b88df
Audrey Tang <audreyt@audreyt.org>
2014-04-08 19:22:01 +0000

    * Sort with .published to work with articles edited after posting

--- js/feed.js
+++ js/feed.js
@@ -1,11 +1,11 @@
 var feed_url = 'https://ethercalc.org/static/proxy/fumao.word.live.json'
-// var feed_url = './fumao.word.live.json'
 var forum_url = 'https://ethercalc.org/static/proxy/enews.json'
 
 $(document).ready(function() {
   $.getJSON(forum_url, function(data){
-    news = $.map(data.entry, function(entry){ return entry }).slice(-3)
-    $.each(news, function(_, news) {
+    news = $.map(data.entry, function(entry){ return entry })
+    news.sort(function(a, b){ return(a.published > b.published) })
+    $.each(news.slice(-3), function(_, news) {
       time = new Date(news.published)
       date_string = addZeros(time.getMonth() + 1) + '/' + addZeros(time.getDate())
       time_string = addZeros(time.getHours()) + ':' + addZeros(time.getMinutes())
@@ -19,7 +19,7 @@ $(document).ready(function() {
   })
 
   $.getJSON(feed_url, function(data){
-    news = data.slice(data.length - 5, data.length)
+    news = data.slice(-5)
     $.each(news, function(_, news) {
       datetime_string = news.time.replace(/^\d+-|:\d+$/g, '')
       $('.livefeed h4').after('<p><time>' + datetime_string + '</time> ' + news.msg + '</p>')
